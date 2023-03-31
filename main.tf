cc13a5f5dd2b1c36e3257dc1e6b13bedf357734e
Your Name <you@example.com>
2023-03-31 17:58:55 +0000

    ttetr

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.35"
+    }
+  }
+}
+
+provider "snowflake" {
+  role  = "SYSADMIN"
+  region   = "eu-west-2.aws"
+
+}
+
+resource "snowflake_database" "db" {
+  name     = "TF_DEMO"
+}
+
+resource "snowflake_warehouse" "warehouse" {
+  name           = "TF_DEMO"
+  warehouse_size = "large"
+
+  auto_suspend = 60
+}
